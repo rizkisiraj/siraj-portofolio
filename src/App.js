e3afb879c88c7bd5279f6e867ed75a2f220268fe
rizkisiraj <jomirwa@gmail.com>
2022-07-15 11:05:33 +0700

    adding suspense

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import Navigation from "./routes/navigation/navigation.component";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Blog from "./routes/blog/blog.component";
-import Project from "./routes/projects/project.component";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./routes/home/home.component"))
+const Blog = lazy(() => import("./routes/blog/blog.component"))
+const Project = lazy(() => import("./routes/projects/project.component"))
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="projects" element={<Project />} />
+        <Route index element={
+        <Suspense fallback={<div>loading...</div>}>
+          <Home />
+        </Suspense>
+        } />
+        <Route path="blog" element={
+          <Suspense fallback={<div>loading...</div>}>
+          <Blog />
+        </Suspense>
+        } />
+        <Route path="projects" element={ 
+          <Suspense fallback={<div>loading...</div>}>
+          <Project />
+        </Suspense>
+        } />
       </Route>
     </Routes>
   );
