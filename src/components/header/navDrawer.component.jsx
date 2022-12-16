d9d97e9708ea5897200cc68014acb83e5059dd45
rizkisiraj <jomirwa@gmail.com>
2022-12-16 11:13:26 +0700

    fixing switch

--- src/components/header/navDrawer.component.jsx
+++ src/components/header/navDrawer.component.jsx
@@ -12,12 +12,6 @@ const footerElements = [
       text: "Github"
   },
   {
-      id: "twitter",
-      link: "https://twitter.com/fatbritishian",
-      icon: <AiOutlineTwitter className="mx-auto" />,
-      text: "Twitter"
-  },
-  {
       id: "linkedin",
       link: "https://www.linkedin.com/in/muhammad-rizki-siraj-1b045b21a/",
       icon: <FaLinkedin className="mx-auto" />,
@@ -55,7 +49,7 @@ const NavDrawer = ({ setTheme, theme, type, setShow }) => {
       </div>
       <div className="py-4">
         <label className="inline-flex relative items-center cursor-pointer">
-          <input type="checkbox" onChange={() => setTheme(!theme)} value="" className="sr-only peer" />
+          <input type="checkbox" onChange={() => setTheme(!theme)} checked={theme === true ? true : false} className="sr-only peer" />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           <span className="ml-3 text-sm font-medium text-white dark:text-gray-300">Mode</span>
         </label>
